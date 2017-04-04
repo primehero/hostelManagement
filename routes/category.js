7848917d51ab4116c129c9dc29e87d85311f1920
Rj <ramachandrajr3@gmail.com>
2017-04-04 19:44:31 +0530

    update 2

--- routes/category.js
+++ routes/category.js
@@ -6,32 +6,22 @@ var Category = require("../models/category");
 routes.get("/", (req, res) => {
 	Category.find({}, (err, foundCategories) => {
 		if (err)
-			res.sendResponse({
-				errorFlag: true,
-				errorMsg: err
-			});
-		// Helps making content false incase there is no content.
-		if (foundCategories.length === 0) 
-			foundCategories = null;
-		res.sendResponse({
-			successFlag: true,
-			content: foundCategories
-		});
+			req.flash("error", err);
+		res.render("category/index", { categories : foundCategories });
 	});
+});
+
+// NEW route
+routes.get("/new", (req, res) => {
+	res.render("category/new");
 });
 
 // SHOW route
 routes.get("/:id", (req, res) => {
 	Category.findById(req.params.id, (err, foundCategory) => {
 		if (err)
-			res.sendResponse({
-				errorFlag: true,
-				errorMsg: err
-			});
-		res.sendResponse({
-			successFlag: true,
-			content: foundCategory
-		});
+			req.flash("error", err);
+		res.render("index");
 	});
 });
 
@@ -39,30 +29,24 @@ routes.get("/:id", (req, res) => {
 routes.post("/", (req, res) => {
 	Category.create(req.body, (err, createdCategory) => {
 		if (err)
-			res.sendResponse({
-				errorFlag: true,
-				errorMsg: err 
-			});
-		res.sendResponse({
-			successFlag: true,
-			successMsg: "Created a new Category " + createdCategory.name
-		});
+			req.flash('error', err);
+		else
+			req.flash('success', "Created a new category " + createdCategory.name);
+		res.redirect('/category');
 	});
 });
+
+// 
 
 
 // UPDATE route
 routes.put("/:id", (req, res) => {
 	Category.findByIdAndUpdate(req.params.id, { $set : req.body }, (err, updatedCategory) => {
 		if (err)
-			res.sendResponse({
-				errorFlag: true,
-				errorMsg: err 
-			});
-		res.sendResponse({
-			successFlag: true,
-			successMsg: "Updated category " + updatedCategory.name
-		});
+			req.flash('error', err);
+		else
+			req.flash('success', "Updated Category " + category.name);
+		res.redirect("/category/" + req.params.id);
 	});
 });
 
@@ -71,14 +55,10 @@ routes.put("/:id", (req, res) => {
 routes.delete("/:id", (req, res) => {
 	Category.findByIdAndRemove(req.params.id, (err) => {
 		if (err)
-			res.sendResponse({
-				errorFlag: true,
-				errorMsg: err 
-			});
-		res.sendResponse({
-			successFlag: true,
-			successMsg: "Removed a new Category Successfully!"
-		});
+			req.flash('error', err);
+		else
+			req.flash('success', "Deleted Category Successfully");
+		res.redirect("/category");
 	});
 });
 
